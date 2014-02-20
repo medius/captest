9175164df2bec00631adf0d1c5618a1066c6f528
Puru Choudhary <puruchoudhary@gmail.com>
2014-02-20 15:38:38 -0800

    cap change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'captest'
+set :repo_url, 'git@github.com:medius/captest.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
